676bd28d347246b6501cf43c4eb9b582dca10833
James Yang <jamesdaeyang@gmail.com>
2022-08-10 22:43:15 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,17 @@
+// create your loops here.
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('');
+
+for (var i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+console.log('');
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion ', i + '!');
+}
